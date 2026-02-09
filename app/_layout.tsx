446b0b14e20ff4e9c209f22afa0c33e002d6e438
AnaVelosoOliveira <anabeatrizvelosodeoliveira@gmail.com>
2026-02-09 11:15:05 -0300

    Add files via upload

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,18 @@
 import { Stack } from 'expo-router';
 
 
+export const unstable_settings = {
+  anchor: '(tabs)',
+};
+
 export default function RootLayout() {
 
-  return (
-  
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name= "list" options={{ headerShown: false}} />
 
+  return (
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown:  false }} />
       </Stack>
-  )
+    
+  );
 }
